2ed7b57c8b211d8f58753e5e0d3144da49f65648
artem <artem.proskurin@kiwibank.co.nz>
2025-03-14 11:46:06 +1300

    Configuration added

--- SettingsDemo/Program.cs
+++ SettingsDemo/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace SettingsDemo
 {
     public class Program
@@ -8,6 +10,9 @@ namespace SettingsDemo
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            SiteConfiguration sc = builder.AddConfiguration<SiteConfiguration>();
+            builder.Services.AddSingleton<SiteConfiguration>(x => sc);
 
             var app = builder.Build();
 
@@ -28,5 +33,103 @@ namespace SettingsDemo
 
             app.Run();
         }
+    }
+
+    public class SiteConfiguration
+    {
+        public String Param1 { get; set; }
+
+        public String Param2 { get; set; }
+
+        public SiteConfiguration()
+        {
+
+        }
+    }
+
+    public static class WebApplicationBuilderExtensions
+    {
+        public static T AddConfiguration<T>(this WebApplicationBuilder builder) where T : class, new()
+        {
+            List<String> props = Functions.getProperties<T>();
+            Dictionary<String, String> prps = new();
+
+            foreach (String prop in props)
+            {
+                IConfigurationSection? tsect = builder.Configuration.GetSection($"Config:{prop}");
+                if (tsect.Value == null)
+                {
+                    throw new Exception($"Configuration error: add {prop} key to appsettings/azure config");
+                }
+                prps.Add(prop, tsect.Value);
+            }
+
+            T temp = new T();
+            foreach (KeyValuePair<String, String> prop in prps)
+            {
+                PropertyInfo? p = typeof(T).GetProperty(prop.Key);
+                p.SetValue(temp, prop.Value);
+            }
+            //ToDo:  refactor this
+            builder.Services.Configure<T>(x =>
+            {
+                foreach (KeyValuePair<String, String> prop in prps)
+                {
+                    PropertyInfo? p = typeof(T).GetProperty(prop.Key);
+                    p.SetValue(x, prop.Value);
+                }
+            });
+            return temp;
+        }
+    }
+
+    public static class Functions
+    {
+        public static List<String> getProperties<T>()
+        {
+            List<String> r = new List<string>();
+            Type myType = typeof(T);
+            PropertyInfo[] properties = myType.GetProperties();
+            foreach (PropertyInfo p in properties)
+            {
+                var temp = p.Name;
+                r.Add(temp);
+            }
+            return r;
+        }
+
+        public static DirectoryInfo? TryGetSolutionDirectoryInfo(string? currentPath = null)
+        {
+            DirectoryInfo? directory = new DirectoryInfo(currentPath ?? Directory.GetCurrentDirectory());
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+            return directory;
+        }
+
+        public static T? GetConfiguration<T>(String ProjectName) where T : class, new()
+        {
+            DirectoryInfo? dir = TryGetSolutionDirectoryInfo();
+            if (dir != null)
+            {
+                String CurDir = Path.Combine(dir.ToString(), ProjectName);
+                IConfigurationRoot Configuration = new ConfigurationBuilder().SetBasePath(CurDir).AddJsonFile("appsettings.json").Build();
+                T res = new T();
+                if (Configuration != null)
+                {
+                    var builder = WebApplication.CreateBuilder();
+                    builder.Configuration.AddConfiguration(Configuration);
+                    T temp = builder.AddConfiguration<T>();
+                    return temp;
+                }
+                return res;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
---
+++ SettingsForms/Form1.cs
@@ -0,0 +1,17 @@
+using SettingsDemo;
+
+namespace SettingsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SiteConfiguration? sc = Functions.GetConfiguration<SiteConfiguration>("SettingsDemo");
+        }
+    }
+}
